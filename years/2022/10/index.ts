f6edf1b640a7ad05059f51b55de651ec09af4426
Rob Thomas <robthomas68@gmail.com>
2022-12-12 23:45:19 -0500

    2022 day 10

---
+++ years/2022/10/index.ts
@@ -0,0 +1,424 @@
+import _, { range } from "lodash";
+import * as util from "../../../util/util";
+import * as test from "../../../util/test";
+import chalk from "chalk";
+import { log, logSolution, trace } from "../../../util/log";
+import { performance } from "perf_hooks";
+import ScreenManager from "inquirer/lib/utils/screen-manager";
+
+const YEAR = 2022;
+const DAY = 10;
+
+// solution path: /home/rob/Workspaces/advent-of-code/years/2022/10/index.ts
+// data path    : /home/rob/Workspaces/advent-of-code/years/2022/10/data.txt
+// problem url  : https://adventofcode.com/2022/day/10
+
+async function p2022day10_part1(input: string, ...params: any[]) {
+	const lines: string[] = input.split("\n");
+	let x = 1;
+	let cycle = 1;
+	let totalSignalStrength = 0;
+
+	function doCycle() {
+		if ([20, 60, 100, 140, 180, 220].includes(cycle)) {
+			const s = cycle * x;
+			totalSignalStrength += s;
+		}
+		cycle += 1;
+	}
+
+	for (const [i, line] of lines.entries()) {
+		const [op, val] = line.split(" ");
+		doCycle();
+		if (op === "addx") {
+			doCycle();
+			x += Number(val);
+		}
+	}
+	return totalSignalStrength;
+}
+
+async function p2022day10_part2(input: string, ...params: any[]) {
+	const lines: string[] = input.split("\n");
+	let pixels: string = "";
+	let x = 1;
+	let cycle = 1;
+
+	function doCycle() {
+		if (_.includes([x - 1, x, x + 1], cycle - 1)) {
+			pixels += "#";
+		} else {
+			pixels += ".";
+		}
+
+		cycle += 1;
+		if (cycle > 40) {
+			cycle = 1;
+			pixels += "\n";
+		}
+	}
+
+	for (const [i, line] of lines.entries()) {
+		const [op, val] = line.split(" ");
+
+		doCycle();
+
+		if (op === "addx") {
+			doCycle();
+			x += Number(val);
+		}
+	}
+	return pixels.trim();
+}
+
+async function run() {
+	const part1tests: TestCase[] = [
+		{
+			input: `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`,
+			extraArgs: [],
+			expected: `13140`,
+		},
+	];
+	const part2tests: TestCase[] = [
+		{
+			input: `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`,
+			extraArgs: [],
+			expected: `##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....`,
+		},
+	];
+
+	// Run tests
+	test.beginTests();
+	await test.section(async () => {
+		for (const testCase of part1tests) {
+			test.logTestResult(testCase, String(await p2022day10_part1(testCase.input, ...(testCase.extraArgs || []))));
+		}
+	});
+	await test.section(async () => {
+		for (const testCase of part2tests) {
+			test.logTestResult(testCase, String(await p2022day10_part2(testCase.input, ...(testCase.extraArgs || []))));
+		}
+	});
+	test.endTests();
+
+	// Get input and run program while measuring performance
+	const input = await util.getInput(DAY, YEAR);
+
+	const part1Before = performance.now();
+	const part1Solution = String(await p2022day10_part1(input));
+	const part1After = performance.now();
+
+	const part2Before = performance.now();
+	const part2Solution = String(await p2022day10_part2(input));
+	const part2After = performance.now();
+
+	logSolution(10, 2022, part1Solution, part2Solution);
+
+	log(chalk.gray("--- Performance ---"));
+	log(chalk.gray(`Part 1: ${util.formatTime(part1After - part1Before)}`));
+	log(chalk.gray(`Part 2: ${util.formatTime(part2After - part2Before)}`));
+	log();
+}
+
+run()
+	.then(() => {
+		process.exit();
+	})
+	.catch(error => {
+		throw error;
+	});
